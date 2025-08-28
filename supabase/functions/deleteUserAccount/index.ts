8f62f83d0f4ae8e6162bce736d168855a5e18de1
Joe Davault <joe.davault@gmail.com>
2025-08-27 23:28:17 -0700

    Updated index.ts

--- supabase/functions/deleteUserAccount/index.ts
+++ supabase/functions/deleteUserAccount/index.ts
@@ -6,8 +6,9 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers':
-    'authorization, x-client-info, apikey, content-type',
+    'authorization, x-client-info, apikey, content-type, x-application-name, x-supabase-api-version, accept, accept-language, cache-control, pragma',
   'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  'Access-Control-Max-Age': '86400',
 };
 
 serve(async (req) => {
