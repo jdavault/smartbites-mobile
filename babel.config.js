7102efe25ee29de453fde778beee9d3c9558e97e
Joe Davault <joe.davault@gmail.com>
2025-09-07 07:00:08 -0700

    Simplify to use direct process.env approach

--- babel.config.js
+++ babel.config.js
@@ -3,16 +3,6 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      [
-        'module:react-native-dotenv',
-        {
-          moduleName: '@env',
-          path: `.env.${process.env.EXPO_PUBLIC_APP_ENV || process.env.NODE_ENV || 'development'}`,
-          safe: false,
-          allowUndefined: true,
-          verbose: false,
-        },
-      ],
       'react-native-reanimated/plugin',
     ],
   };
